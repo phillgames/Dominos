bd9046d8f108c9dbe392f0310cc3d67230d59be6
phillsearle <102220888+phillgames@users.noreply.github.com>
2025-02-27 11:34:59 +0100

    testing link

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
